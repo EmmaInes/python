88ac43bfb9e0b2226a25a257a785b894b952fe7f
Emma <emma.wahnish@groovinads.com>
2025-09-03 11:15:05 -0300

    snowflake stages and file formats

---
+++ orc-file-format.sql
@@ -0,0 +1,6 @@
+USE IPEDS;
+-- Create file format to load ORC file 
+CREATE OR REPLACE FILE FORMAT ff_IPEDS_ORC
+			TYPE =ORC TRIM_SPACE = TRUE;
+-- Create stage area to load ORC file 
+CREATE OR REPLACE stage IPEDS_IC FILE_FORMAT = ff_IPEDS_ORC;
---
+++ parquet-file-format.sql
@@ -0,0 +1,6 @@
+USE IPEDS;
+-- Create Parquet file format
+CREATE OR REPLACE  FILE FORMAT ff_IPEDS_Parquet
+			TYPE =PARQUET TRIM_SPACE = TRUE;
+-- Create stage area to load Parquet file 
+CREATE OR REPLACE stage IPEDS_ADM FILE_FORMAT = ff_IPEDS_Parquet;
